5abc257fcd299c8fada09f4aeb59e3461d110004
Dell <Dell@DESKTOP-EF6UDTL>
2024-02-29 15:28:24 +0530

    Add project files.

---
+++ projectweb/Controllers/AccountController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using projectweb.Models;
+using System.Web.Security;
+
+namespace projectweb.Controllers
+{
+    [AllowAnonymous]
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Models.Membership model)
+        {
+            using (var context = new CrudEntities())
+            {
+                bool isValid = context.login.Any(x => x.Username == model.Username && x.Password == model.Password);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username,false);
+                    return RedirectToAction("Index","employees");
+                }
+                ModelState.AddModelError("", "Invalid Username and Password!!");
+                return View();
+            }
+            
+        }
+
+        public ActionResult SignUp()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult SignUp(login model)
+        {
+            try
+            {
+                using (var context = new CrudEntities())
+                {
+                    context.login.Add(model);
+                    context.SaveChanges();
+                }
+            } catch (Exception e ) 
+            {
+                ViewBag.message = "plzz fill all the fields!";
+                return View();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ projectweb/Controllers/employeesController.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using projectweb;
+
+namespace projectweb.Controllers
+{
+    [Authorize]
+    public class employeesController : Controller
+    {
+        private CrudEntities db = new CrudEntities();
+
+        // GET: employees
+        public ActionResult Index()
+        {
+            return View(db.employee.ToList());
+        }
+
+        // GET: employees/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // GET: employees/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: employees/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Username,Email,Password,Department,Location,Salary")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.employee.Add(employee);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(employee);
+        }
+
+        // GET: employees/Edit/5
+        public ActionResult Edit(string id)
+        {
+           
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: employees/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Username,Email,Password,Department,Location,Salary")] employee employee)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(employee).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(employee);
+        }
+
+        // GET: employees/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            employee employee = db.employee.Find(id);
+            if (employee == null)
+            {
+                return HttpNotFound();
+            }
+            return View(employee);
+        }
+
+        // POST: employees/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            employee employee = db.employee.Find(id);
+            db.employee.Remove(employee);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+        [AllowAnonymous]
+        public ActionResult About()
+        {
+            return View();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
